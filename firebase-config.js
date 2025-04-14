27a15607b9aca39c17bd8c6f67d33d29d3dae309
Loes <all.game.11king@gmail.com>
2025-04-14 14:08:59 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTpo54-oavDjwLADyqzmSmKmnX7_MFGOM",
+  authDomain: "virtutrust-e24d3.firebaseapp.com",
+  databaseURL: "https://virtutrust-e24d3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "virtutrust-e24d3",
+  storageBucket: "virtutrust-e24d3.firebasestorage.app",
+  messagingSenderId: "638898992846",
+  appId: "1:638898992846:web:c98cea98e17483c60dc29f",
+  measurementId: "G-NV067JK062"
+};
+
+firebase.initializeApp(firebaseConfig);
